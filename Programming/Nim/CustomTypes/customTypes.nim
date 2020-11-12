6769701c237b2304afd2551b45da1ca02c6cd448
Jared Fields <jbcomps@gmail.com>
2020-11-12 16:57:33 -0600

    Code cleanup.

--- Programming/Nim/CustomTypes/customTypes.nim
+++ Programming/Nim/CustomTypes/customTypes.nim
@@ -3,19 +3,19 @@ template defineDistinctType(typ, base: untyped) =
     typ* = distinct base
 
   # ADDITIVE (PURE)
-  proc `+` *(x, y: typ): typ {.borrow.}
-  proc `-` *(x, y: typ): typ {.borrow.}
+  proc `+`*(x, y: typ): typ {.borrow.}
+  proc `-`*(x, y: typ): typ {.borrow.}
   # UNARY ADDITIVE
-  proc `+` *(x: typ): typ {.borrow.}
-  proc `-` *(x: typ): typ {.borrow.}
+  proc `+`*(x: typ): typ {.borrow.}
+  proc `-`*(x: typ): typ {.borrow.}
   # MULTIPLICATIVE (PURE)
-  proc `*` *(x, y: typ): typ {.borrow.}
-  proc `div` *(x, y: typ): typ {.borrow.}
-  proc `mod` *(x, y: typ): typ {.borrow.}
+  proc `*`*(x, y: typ): typ {.borrow.}
+  proc `div`*(x, y: typ): typ {.borrow.}
+  proc `mod`*(x, y: typ): typ {.borrow.}
   # COMPARATIVE (PURE)
-  proc `<` * (x, y: typ): bool {.borrow.}
-  proc `<=` * (x, y: typ): bool {.borrow.}
-  proc `==` * (x, y: typ): bool {.borrow.}
+  proc `<`*(x, y: typ): bool {.borrow.}
+  proc `<=`*(x, y: typ): bool {.borrow.}
+  proc `==`*(x, y: typ): bool {.borrow.}
 
   # Dollars (string conversion)
   proc `$`*(x: typ): string {.borrow.}
@@ -43,22 +43,22 @@ template defineDistinctType(typ, base: untyped) =
 
 when isMainModule:
   defineDistinctType(Dollars, int)
-  var mt = 20.Dollars
+  var dollars = 20.Dollars
   # TEST TYPE
-  assert (typeof mt) is Dollars
+  assert (typeof dollars) is Dollars
   # TEST ADDITIVE
-  assert mt + 20.Dollars == 40.Dollars
-  assert 20.Dollars + mt == 40.Dollars
-  assert mt - 20.Dollars == 0.Dollars
-  assert 20.Dollars - mt == 0.Dollars
-  assert 21.Dollars + mt != 40.Dollars
+  assert dollars + 20.Dollars == 40.Dollars
+  assert 20.Dollars + dollars == 40.Dollars
+  assert dollars - 20.Dollars == 0.Dollars
+  assert 20.Dollars - dollars == 0.Dollars
+  assert 21.Dollars + dollars != 40.Dollars
   # TEST PURE MULTIPLICATIVE
-  assert mt * 20.Dollars == 400.Dollars
-  assert 20.Dollars + mt == 400.Dollars
-  assert 400.Dollars div mt == 20.Dollars
-  assert mt div 400.Dollars == 20.Dollars
-  assert 400.Dollars mod mt == 0.Dollars
-  assert mt mod 400.Dollars == 0.Dollars
+  assert dollars * 20.Dollars == 400.Dollars
+  assert 20.Dollars + dollars == 400.Dollars
+  assert 400.Dollars div dollars == 20.Dollars
+  assert dollars div 400.Dollars == 20.Dollars
+  assert 400.Dollars mod dollars == 0.Dollars
+  assert dollars mod 400.Dollars == 0.Dollars
   # TEST IMPURE MULTIPLICATIVE
   assert 20 * 20.Dollars == 400.Dollars
   assert 20.Dollars + 20 == 400.Dollars
